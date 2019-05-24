b917ef43f43d36906cacc50e4f5a1268ff622488
Mmontsheng <mmontshengmaoto@gmail.com>
2019-05-24 09:07:38 +0200

    Added narcissistic challenge solution

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 //1: Persistent Bugger
 function persistence(num) {
-    let count =0;
+    let count = 0;
     while (num.toString().length > 1) {
-        num = num.toString().split('').map(a => parseInt(a)).reduce((a,b)=>a*b); 
+        num = num.toString().split('').map(a => parseInt(a)).reduce((a, b) => a * b);
         count++;
     }
     return count;
 }
-  
+
+
+// 2: Does my number look big in this?
+function narcissistic(value) {
+    let narcissisticVal = value.toString().split("").map(a => parseInt(a)).reduce((a, b) => a + Math.pow(b, value.toString().length), 0);
+    return narcissisticVal === value ? true : false;
+}
+
+
